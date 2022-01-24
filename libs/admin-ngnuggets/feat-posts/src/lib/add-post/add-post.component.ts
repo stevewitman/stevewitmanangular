a8e1214cea6393489338b187f1fad816a9c201b8
Steve Witman <stevewitman@gmail.com>
2022-01-24 14:22:09 -0700

    [admin-ngnuggets] feat(posts) add autocomplete with chips for tags
      - add autocomplete with chips to tags field of add-post component
    \

--- libs/admin-ngnuggets/feat-posts/src/lib/add-post/add-post.component.ts
+++ libs/admin-ngnuggets/feat-posts/src/lib/add-post/add-post.component.ts
@@ -1,11 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { map, Observable, startWith } from 'rxjs';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
 
+export interface Tag {
+  category: string;
+  tag: string;
+  alt: string;
+}
 @Component({
   selector: 'admin-ngnuggets-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.scss']
+  styleUrls: ['./add-post.component.scss'],
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit {
+  postForm!: FormGroup;
 
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagsCtrl = new FormControl();
+  filteredTags: Observable<string[]>;
+  tags: string[] = [];
+  allTags: string[] = [
+    'AOT',
+    'Accessibility',
+    'Angular Elements',
+    'Angular 13',
+    'Angular Universal',
+    'Angular Workspace',
+    'Animations',
+    'Architecture',
+    'Attribute Binding',
+    'Builders',
+    'CLI',
+    'Change Detection',
+    'Components',
+    'Component Factory',
+  ];
 
+  @ViewChild('tagsInput') tagsInput?: ElementRef<HTMLInputElement>;
+
+  constructor(private fb: FormBuilder) {
+    this.filteredTags = this.tagsCtrl.valueChanges.pipe(
+      startWith(''),
+      map((tags) => (tags ? this._filterTags(tags) : this.tags.slice()))
+    );
+  }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.postForm = this.fb.group({
+      tagsCtrl: [''],
+    });
+  }
+
+  tagAdd(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.tags.push(value);
+    }
+    event.chipInput?.clear();
+    this.tagsCtrl.setValue(null);
+  }
+
+  tagRemove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  tagSelected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.push(event.option.viewValue);
+    if (this.tagsInput) {
+      this.tagsInput.nativeElement.value = '';
+    } 
+    this.tagsCtrl.setValue(null);
+  }
+
+  private _filterTags(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.allTags.filter((tag) =>
+      tag.toLowerCase().includes(filterValue)
+    );
+  }
 }
--- libs/admin-ngnuggets/feat-posts/src/lib/admin-ngnuggets-feat-posts.module.ts
+++ libs/admin-ngnuggets/feat-posts/src/lib/admin-ngnuggets-feat-posts.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
 
 import { SharedUiMaterialModule } from '@stevewitmanangular/shared/ui/material';
 import { AddPostComponent } from './add-post/add-post.component';
@@ -26,7 +28,11 @@ export const adminNgnuggetsFeatPostsRoutes: Route[] = [
   imports: [
     CommonModule,
     RouterModule.forChild(adminNgnuggetsFeatPostsRoutes),
+    FormsModule,
+    ReactiveFormsModule,
     SharedUiMaterialModule,
+    MatAutocompleteModule,
+    MatChipsModule,
     MatFormFieldModule,
     MatInputModule,
   ],
