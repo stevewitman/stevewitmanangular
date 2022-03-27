70d8e643a4f419a65f0daa33d9c4b233ed1879f2
Steve Witman <stevewitman@gmail.com>
2022-03-26 23:00:59 -0600

    [admin-ngnuggets] posts: add-post form initialization and autocompletion
      - add datePostedCtrl and dateSourceCtrl formControlNames
      - initialize thesse form dates
      - add method to decrement or increment dateSource with button clicks
      - add sourceSiteCtrl and sourceUrlCtrl formControlNames
      - add method autocompletes typeCtrl, sourceSiteCtrl, sourceUrlCtrl on url change

--- libs/admin-ngnuggets/posts/feature/add-post/src/lib/add-post.component.ts
+++ libs/admin-ngnuggets/posts/feature/add-post/src/lib/add-post.component.ts
@@ -6,6 +6,7 @@ import { map, startWith, switchMap, tap } from 'rxjs/operators';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatChipInputEvent } from '@angular/material/chips';
+
 import { ref, Storage } from '@angular/fire//storage';
 import { getDownloadURL, uploadBytes } from 'firebase/storage';
 
@@ -26,6 +27,11 @@ export class AddPostComponent implements OnInit {
   typeCtrl: FormControl = new FormControl();
   postUrlCtrl: FormControl = new FormControl();
   tagsCtrl: FormControl = new FormControl();
+  sourceSiteCtrl: FormControl = new FormControl();
+  sourceUrlCtrl: FormControl = new FormControl();
+  todaysDate: Date = new Date();
+  currSourceDate: Date = new Date();
+  sourceDateOffset = 0;
   tagsAll: string[] = [];
   tagsFiltered: Observable<string[]> = of([]);
   tagsSelected: string[] = [];
@@ -37,7 +43,6 @@ export class AddPostComponent implements OnInit {
     { value: 'community' },
     { value: 'release' },
   ];
-  // postTypeSelectedValue = '';
 
   slug = '2022-01-28-B';
 
@@ -46,15 +51,14 @@ export class AddPostComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private storage: Storage) {}
 
   ngOnInit() {
+    this.todaysDate = new Date();    
+    this.currSourceDate = this.todaysDate;
     this.initializeForm();
     this.tagsAll = this.getTags();
     this.tagsFiltered = this.tagsCtrl.valueChanges.pipe(
       startWith(null),
       map((tags) => (tags ? this._filterTags(tags) : this.tagsSelected.slice()))
     );
-    // this.postForm.controls['postUrlCtrl'].valueChanges.subscribe((change) => {
-    //   console.log('postUrlCtrl sub', change); // Value inside the input field as soon as it changes
-    // });
   }
 
   initializeForm(): void {
@@ -62,8 +66,20 @@ export class AddPostComponent implements OnInit {
       typeCtrl: [''],
       postUrlCtrl: [''],
       tagsCtrl: [''],
+      datePostedCtrl: [this.todaysDate.toISOString().split('T')[0]],
+      dateSourceCtrl: [this.todaysDate.toISOString().split('T')[0]],
+      sourceSiteCtrl: [''],
+      sourceUrlCtrl: [''],
     });
   }
+
+  changeSourceDate(value: number) {
+    this.currSourceDate.setDate(this.currSourceDate.getDate() + value );
+    this.postForm.controls['dateSourceCtrl'].setValue(
+      this.currSourceDate.toISOString().split('T')[0]
+    );
+  }
+
 
   getTags() {
     const newArray = tagsData.map((el) => {
@@ -122,33 +138,75 @@ export class AddPostComponent implements OnInit {
     // [video] YouTube
     if (currUrl.indexOf('https://www.youtube.com/watch?') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('video');
-    // [video] YouTube (at current time)
-    } if (currUrl.indexOf('https://youtu.be') !== -1) {
+      this.postForm.controls['sourceSiteCtrl'].setValue('YouTube');
+      this.postForm.controls['sourceUrlCtrl'].setValue('https://youtube.com');
+      // [video] YouTube (at current time)
+    } else if (currUrl.indexOf('https://youtu.be') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('video');
-    // [blog] Dev.to
+      this.postForm.controls['sourceSiteCtrl'].setValue('YouTube');
+      this.postForm.controls['sourceUrlCtrl'].setValue('https://youtube.com');
+      // [blog] Dev.to
     } else if (currUrl.indexOf('https://dev.to/') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('blog');
-    // [blog] Medium
+      this.postForm.controls['sourceSiteCtrl'].setValue('DEV Community');
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://dev.to/search?q=angular&sort_by=published_at&sort_direction=desc'
+      );
+      // [blog] Medium
     } else if (currUrl.indexOf('https://medium.com/') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('blog');
-    // [blog] Hashnode
+      this.postForm.controls['sourceSiteCtrl'].setValue('Medium');
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://medium.com/tag/angular/latest'
+      );
+      // [blog] Hashnode
     } else if (currUrl.indexOf('hashnode.dev/') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('blog');
-    // [blog] Telerik
+      this.postForm.controls['sourceSiteCtrl'].setValue('Hashnode');
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://hashnode.com/n/angular'
+      );
+      // [blog] Telerik
     } else if (currUrl.indexOf('https://www.telerik.com/blogs') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('blog');
-    // [podcast] Adventures In Angular
+      this.postForm.controls['sourceSiteCtrl'].setValue('Telerik Blogs');
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://www.telerik.com/blogs'
+      );
+      // [podcast] Adventures In Angular
     } else if (currUrl.indexOf('https://adventuresinangular.com/') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('podcast');
-    // [podcast] Angular Experience
-    } else if (currUrl.indexOf('https://www.spreaker.com/user/14532324/') !== -1) {
+      this.postForm.controls['sourceSiteCtrl'].setValue(
+        'Adventures in Angular'
+      );
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://adventuresinangular.com'
+      );
+      // [podcast] Angular Experience
+    } else if (
+      currUrl.indexOf('https://www.spreaker.com/user/14532324/') !== -1
+    ) {
       this.postForm.controls['typeCtrl'].setValue('podcast');
-    // [podcast] WebRush
+      this.postForm.controls['sourceSiteCtrl'].setValue('Angular Experience');
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://angular-experience.web.app/episodes'
+      );
+      // [podcast] WebRush
     } else if (currUrl.indexOf('https://webrush.io/episodes/') !== -1) {
       this.postForm.controls['typeCtrl'].setValue('podcast');
-    // [release] Angular GitHub
-    } else if (currUrl.indexOf('https://github.com/angular/angular/releases') !== -1) {
+      this.postForm.controls['sourceSiteCtrl'].setValue('WsbRush');
+      this.postForm.controls['sourceUrlCtrl'].setValue('https://webrush.io');
+      // [release] Angular GitHub
+    } else if (
+      currUrl.indexOf('https://github.com/angular/angular/releases') !== -1
+    ) {
       this.postForm.controls['typeCtrl'].setValue('release');
+      this.postForm.controls['sourceSiteCtrl'].setValue(
+        'GitHub Angular Releases'
+      );
+      this.postForm.controls['sourceUrlCtrl'].setValue(
+        'https://github.com/angular/angular/releases'
+      );
     }
   }
 
